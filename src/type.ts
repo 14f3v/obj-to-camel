290ceaf8f8fc78f6e242e0136f28797bb0a60f31
14f3v <khemphet.souvannaphasy@gmail.com>
2024-03-02 21:19:28 +0700

    applied complex nested object
    - applied complex nested object support

--- src/type.ts
+++ src/type.ts
@@ -2,4 +2,10 @@ type CamelCase<T extends string> = T extends `${infer First}_${infer Rest}`
     ? `${Lowercase<First>}${Capitalize<CamelCase<Rest>>}`
     : Lowercase<T>;
 
-export type { CamelCase };
+type ParentNested<T> = {
+    [K in keyof T as CamelCase<K & string>]: T[K] extends object
+    ? ParentNested<T[K]>
+    : T[K];
+};
+
+export type { ParentNested as CamelCase };
