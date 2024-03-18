d52c27382563e84f3961f998ab351e2913068703
Khemphet SOUVANNAPHASY <khemphet.souvannaphasy@gmail.com>
2024-03-18 12:36:24 +0700

    update fix return empty object insteand of nuill

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ export default function ObjectConvertionKeysToCamelCase<T>(obj: T): CamelCase<T>
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
             const camelKey = key.toLowerCase().replace(/_([a-z])/g, (match, letter) => letter.toUpperCase());
             if (typeof obj[key] == 'object') {
-                camelObj[camelKey.charAt(0).toLowerCase() + camelKey.slice(1)] = ObjectConvertionKeysToCamelCase(obj[key]);
+                if (!obj[key]) camelObj[camelKey.charAt(0).toLowerCase() + camelKey.slice(1)] = obj[key]
+                else camelObj[camelKey.charAt(0).toLowerCase() + camelKey.slice(1)] = ObjectConvertionKeysToCamelCase(obj[key]);
             }
             else camelObj[camelKey.charAt(0).toLowerCase() + camelKey.slice(1)] = obj[key];
         }
