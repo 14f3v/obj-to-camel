6a688ee322563833daeb1118f366021c85849ba5
Khemphet SOUVANNAPHASY <khemphet.souvannaphasy@gmail.com>
2024-03-21 09:56:00 +0700

    apply fix invalid validation of instance type

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default function ObjectConvertionKeysToCamelCase<T>(obj: T): CamelCase<T>
     for (const key in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
             const camelKey = key.toLowerCase().replace(/_([a-z])/g, (match, letter) => letter.toUpperCase());
-            if (typeof obj[key] == 'object') {
+            if (obj[key] instanceof Object && !(obj[key] instanceof Date)) {
                 if (!obj[key]) camelObj[camelKey.charAt(0).toLowerCase() + camelKey.slice(1)] = obj[key]
                 else camelObj[camelKey.charAt(0).toLowerCase() + camelKey.slice(1)] = ObjectConvertionKeysToCamelCase(obj[key]);
             }
